628c411047587ab7bd804a77f178625697bd26fd
Leyla Bayramli <lbayramli92@gmail.com>
2019-11-22 13:02:32 -0800

    Added name method

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -5,6 +5,11 @@ public class Person {
     int age;
     String firstname;
     private String address;
-    
+    public Person(int age){
+    this.age=age;
+    }
+     public void Name(){
+         System.out.println("Cybertek");
+     }
 
 }
